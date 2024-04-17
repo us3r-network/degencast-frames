057a1303170e44d576c4b7e8ac4fd45fb0524ef0
ttang <ttang@ttang-MacBook-Pro-2021.local>
2024-04-17 15:43:03 +0800

    feat: share

--- src/allowance/finish.tsx
+++ src/allowance/finish.tsx
@@ -2,17 +2,25 @@
 
 import { Button, Env, FrameContext, Frog, TextInput } from "frog";
 import { BlankInput } from "hono/types";
+import { ALLOWANCE_FRAME } from "../lib/env";
 
 export const finishScreen = (
   c: FrameContext<Env, "/:channel/finish", BlankInput>
 ) => {
   const channel = c.req.param("channel");
+  const fid = c.frameData?.fid!;
   return c.res({
     image: `/allowance/images/success.png`,
     intents: [
       <Button action={`/${channel}/buy/share`}>Buy more</Button>,
       <Button action={`/${channel}/check/allowance`}>Allowance</Button>,
-      <Button.Link href="https://degencast.xyz">share</Button.Link>,
+      <Button.Link
+        href={`https://warpcast.com/~/compose?text=${encodeURIComponent(
+          `Buy shares in degencast`
+        )}&embeds[]=${ALLOWANCE_FRAME} + /${channel}/fid/${fid}`}
+      >
+        Share
+      </Button.Link>,
       // <Button.Link href="https://degencast.xyz">Leaderboard</Button.Link>,
     ],
   });
--- src/allowance/start.tsx
+++ src/allowance/start.tsx
@@ -2,6 +2,7 @@
 
 import { Button, Env, FrameContext, Frog, TextInput } from "frog";
 import { BlankInput } from "hono/types";
+import { ALLOWANCE_FRAME } from "../lib/env";
 
 export function start(c: FrameContext<Env, "/:channel", BlankInput>) {
   const channel = c.req.param("channel");
@@ -13,7 +14,13 @@ export function start(c: FrameContext<Env, "/:channel", BlankInput>) {
     intents: [
       <Button action={`/${channel}/buy/share`}>Buy shares</Button>,
       <Button action={`/${channel}/check/allowance`}>Allowance</Button>,
-      <Button.Link href="https://degencast.xyz">Share</Button.Link>,
+      <Button.Link
+        href={`https://warpcast.com/~/compose?text=${encodeURIComponent(
+          `Buy shares in degencast`
+        )}&embeds[]=${ALLOWANCE_FRAME} + /${channel}`}
+      >
+        Share
+      </Button.Link>,
       // <Button.Link href="https://degencast.xyz">Open App</Button.Link>,
     ],
   });
